2685f95c37ee7b4d4451856a0d15174680a94877
Mattias Kuosku <mattias.kuosku@honeybi.se>
2020-08-21 14:06:12 +0200

    Senaste ändringarna för dagens pass

--- src/webapp/landing.cljs
+++ src/webapp/landing.cljs
@@ -1,12 +1,32 @@
-(ns webapp.landing)
+(ns webapp.landing
 
+  (:require [saikyun.mein.dom :as dom]
+            [saikyun.mein.core :refer [render cleanup!]]
+            [saikyun.mein.props :as p]))
+
+
+
+(defonce app-div (dom/get-or-create "app"))
+(def todos (atom ["Köp kaffe" "Köp slatt mjölk"]))
+
+(def view [:div [:h1 "Todos"]
+           (-> (into [:div] (for [t @todos] [:div t]))
+               (p/watch todos (fn [e _ _ new-todos]
+                                (set! (.-innerHTML e) new-todos)
+                                (println "Test"))))
+           (->
+            [:input]
+            (p/spice :mein/on {:change  #(swap! todos conj (.. % -target -value))}))])
+(println view)
+(println (into [1] '("2" "3")))
 (defn start
   []
-  (println "Starting!"))
+  (render app-div view)
+  (println "Starting Jona!"))
 
 (defn init
   []
   (start))
 
 (defn stop
-  [])
+  [] (cleanup! ))
